d6782804f1d69150a5cdd33d55f6d2da8b816dfd
ethan <1787120202@qq.com>
2023-01-11 09:26:38 +0800

    test error

--- lua/modules/tools/package.lua
+++ lua/modules/tools/package.lua
@@ -1,15 +1,15 @@
 local package = require('core.pack').package
 local conf = require('modules.tools.config')
 
-package({
-  'nvim-telescope/telescope.nvim',
-  cmd = 'Telescope',
-  config = conf.telescope,
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzy-native.nvim' },
-  },
-})
+-- package({
+--   'nvim-telescope/telescope.nvim',
+--   cmd = 'Telescope',
+--   config = conf.telescope,
+--   dependencies = {
+--     { 'nvim-lua/plenary.nvim' },
+--     { 'nvim-telescope/telescope-fzy-native.nvim' },
+--   },
+-- })
 
 package({
   'glepnir/hlsearch.nvim',
